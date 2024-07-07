1154d1c851bed30df85f83997363b9c6969ba666
EsdrasC24 <josuequeraleswd2420@gmail.com>
2024-07-06 23:43:32 -0400

    agregar enlazado a vistas

--- qr/views.py
+++ qr/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def principal(request):
+  return render(request, 'qr/principal.html')
+  
+def codificar(request):
+  return render(request, 'qr/codificar.html')
+  
+def codificar_resultado(request):
+  return render(request, 'qr/codificar_resultado.html')
+  
+def creditos(request):
+  return render(request, 'qr/creditos.html')
+  
+def decodificar(request):
+  return render(request, 'qr/decodificar.html')
